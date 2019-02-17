922d81df642d57779a62066a28b22522d2600e15
Jocomol <joelmeier08@gmail.com>
2019-02-17 21:01:19 +0100

    try

--- scrLib/wsPart.py
+++ scrLib/wsPart.py
@@ -1,11 +1,11 @@
-##This is a superclass for the classes thermo and dbConnector 
+##This is a superclass for the classes thermo and dbConnector
 class wsPart(object):
     logger = None
     name = ""
     def __init__(self, name, logger):
         self.logger = logger
         self.name = name
-        self.logger.writeLog(self, "created")
+        self.logger.writeLog(self, message=str("created"))
 
     def getName(self):
         return self.name
