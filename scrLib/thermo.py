85fb11be1132ef702e87211a58cec503d9a44d0f
Jocomol <joelmeier08@gmail.com>
2019-02-17 16:10:17 +0100

    Fixed Indent Error

--- scrLib/thermo.py
+++ scrLib/thermo.py
@@ -5,10 +5,10 @@ class thermo:
     file = '/sys/bus/w1/devices/28-00000833e8ff/w1_slave'
     def __init__(self):
         ##Run test to see if functional
-        pass
+        functional = True
 
     def read(self):
-    	fileobject = open(file)
-    	filecontent = fileobject.read()
-    	fileobject.close()
+        fileobject = open(file)
+        filecontent = fileobject.read()
+        fileobject.close()
         return filecontent
