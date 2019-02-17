ec2b8277bccb922c094a242c578c2f3d2c9d7619
Jocomol <joelmeier08@gmail.com>
2019-02-17 18:14:48 +0100

    fixed wrong name error

--- scrLib/dbConnector.py
+++ scrLib/dbConnector.py
@@ -2,7 +2,7 @@ import sqlite3
 
 class dbConnector:
     def DBInsertMeasurement(self, dataArray):
-        connection = sqlite3.connect('/var/wheaterstation/data/wheater.db')
+        conn = sqlite3.connect('/var/wheaterstation/data/wheater.db')
         c = conn.cursor()
         c.execute('insert into measurement (DateTime, temperature_C, temperature_F, temperature_K) VALUES (?,?,?,?)', dataArray)
         conn.commit()
