a1f769b3563a81364033b39b01e3e02c88dce681
Jocomol <joelmeier08@gmail.com>
2019-02-17 17:31:56 +0100

    indent error fixed

--- scrLib/wsControl.py
+++ scrLib/wsControl.py
@@ -7,13 +7,12 @@ thermo = thermo()
 
 def tempMeassure():
     tempData = thermo.read()
-    #TODO Parse and Calculate K, F
-	value1 = tempData.split("\n")
-	value2 = value1[1]
-	value3 = value2.split(" ")
-	value4 = value3[9]
-	value5 = value4.split("=")
-	temp = float(value5[1])
+    value1 = tempData.split("\n")
+    value2 = value1[1]
+    value3 = value2.split(" ")
+    value4 = value3[9]
+    value5 = value4.split("=")
+    temp = float(value5[1])
     cel = temp / 1000
     fah = pytemperature.c2f(cel)
     kel = pytemperature.k2c(cel)
