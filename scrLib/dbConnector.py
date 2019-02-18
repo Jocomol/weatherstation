db84b1a4ce26e20a68210204c78ae8b7c652ca4e
Jocomol <joelmeier08@gmail.com>
2019-02-18 19:46:56 +0100

    removed error handling to see error

--- scrLib/dbConnector.py
+++ scrLib/dbConnector.py
@@ -6,13 +6,9 @@ class DBConnector(WsPart):
         super().__init__(name, logger)
 
     def database_insert_measurement(self, measurement):
-        try:
-            connection = sqlite3.connect('/var/wheaterstation/data/wheater.db')
-            cursor = connection.cursor()
-            cursor.execute('insert into measurement (DateTime, temperature_C, temperature_F, temperature_K) VALUES (?,?,?,?)', dataArray)
-            self.logger.writeLog(self, str('Write',measurement,'into Database'))
-        except Exception as e:
-            print(e.message)
-        finally:
-            connection.commit()
-            connection.close()
+        connection = sqlite3.connect('/var/wheaterstation/data/wheater.db')
+        cursor = connection.cursor()
+        cursor.execute('insert into measurement (DateTime, temperature_C, temperature_F, temperature_K) VALUES (?,?,?,?)', dataArray)
+        self.logger.writeLog(self, str('Write',measurement,'into Database'))
+        connection.commit()
+        connection.close()
