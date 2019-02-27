3b5f04ff00f46e656669c354bf42b30f965c31cb
Jocomol <joelmeier08@gmail.com>
2019-02-27 20:21:49 +0100

    rearrangement of the scrlib

--- scrLib/thermo.py
+++ scrLib/thermo.py
@@ -1,21 +1,24 @@
 from wsPart import WsPart
 import os
 import sys
+from w1thermsensor import W1ThermSensor
+import datetime
 
 
-class Thermo(WsPart):  # Reads out the file of the data from the Thermometer
-    functional = False  # Functionality will be added later
-    # file = "/home/joco/Documents/testing.txt"
-    # File where the thermometer writes the data in
-    file = '/sys/bus/w1/devices/28-00000833e8ff/w1_slave'
+class Thermo(WsPart, W1ThermSensor):
 
-    def __init__(self, name, logger):
-        super().__init__(name, logger)  # Calls the wsPart constructor
+    def __init__(self, logger):
+        super().__init__(logger)  # Calls the wsPart constructor
+        self.set_precision(5)
 
-    def read_measurement(self):  # Reads out the file defined as self.file
-        with open(self.file) as fileobject:
-            fileobject = open(self.file)
-            filecontent = fileobject.read()
-            fileobject.close()
-            self.logger.info("Completed Meassuring")
-        return filecontent
+    def read_measurement(self):
+        self.logger.info("Meassuring started")
+        meassuered_data = [str(datetime.datetime.now.().isoformat())]
+        meassuered_data.append(self.get_temperatures([
+                    self.DEGREES_C,
+                    self.DEGREES_F,
+                    self.KELVIN]))
+        self.thermometer_logger.info(
+            "Meassured Temperatures: " +
+            str(meassuered_data))
+        return meassuered_data
--- scrLib/wsControl.py
+++ scrLib/wsControl.py
@@ -1,5 +1,4 @@
 # Imports
-import datetime
 from thermo import Thermo
 from dbConnector import DBConnector
 from wsPart import WsPart
@@ -23,41 +22,9 @@ class Controller():  # Controlls everything and manages the weatherstation
             self.db_connector_logger)
         self.controller_logger.info("All Objects created")
 
-    # Gets the temparature data from the thermometer class
-    def tempMeassure(self):
-        temp_data = self.thermo.read_measurement()
-        value_1 = temp_data.split("\n")
-        value_2 = value_1[1]
-        value_3 = value_2.split(" ")
-        value_4 = value_3[9]
-        value_5 = value_4.split("=")
-        temp = float(value_5[1])
-        cel = temp / 1000
-        fah = pytemperature.c2f(cel)
-        kel = pytemperature.c2k(cel)
-        temp_array = [str(cel), str(fah), str(kel)]
-        return temp_array
-
     def main(self):  # Calls all methods and writes results into the database
-        self.thermometer_logger.info("Meassuring started")
-        data_array = []
-        data_array.append(self.getTime())
-        temp_array = self.tempMeassure()
-        self.thermometer_logger.info(
-            "Meassured Temperatures: " +
-            str(temp_array))
-        data_array.append(temp_array[0])
-        data_array.append(temp_array[1])
-        data_array.append(temp_array[2])
+        data_array = self.thermo.read_measurement()
         self.db_connector.database_insert_measurement(data_array)
-
-    def getTime(self):
-        now = datetime.datetime.now()
-        return str(now.isoformat())
-
-    def getName(self):
-        return self.name
-
 
 if __name__ == "__main__":
     controller = Controller()
