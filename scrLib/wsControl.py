4be6a0cb5b0738d6bbfbb5ef219b948fa59d90a7
Jocomol <joelmeier08@gmail.com>
2019-02-27 20:27:42 +0100

    fixes

--- scrLib/wsControl.py
+++ scrLib/wsControl.py
@@ -16,10 +16,8 @@ class Controller():  # Controlls everything and manages the weatherstation
         self.controller_logger = logging.getLogger("Controller")
         self.thermometer_logger = logging.getLogger("Thermometer")
         self.db_connector_logger = logging.getLogger("Database Connector")
-        self.thermo = Thermo("Thermometer", self.thermometer_logger)
-        self.db_connector = DBConnector(
-            "DBConnector",
-            self.db_connector_logger)
+        self.thermo = Thermo(self.thermometer_logger)
+        self.db_connector = DBConnector(self.db_connector_logger)
         self.controller_logger.info("All Objects created")
 
     def main(self):  # Calls all methods and writes results into the database
